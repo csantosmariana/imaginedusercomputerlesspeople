455740a3380f7caf8f4f997c6687d9bb1617b517
csantosmariana <56931876+csantosmariana@users.noreply.github.com>
2020-01-23 16:48:46 +0000

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,102 @@
+$(document).mousemove(function(e){
+            const height = $('#image1').height() / 10;
+            const width = $('#image1').width() / 28;
+                $("#image1").css({
+                left:e.pageX-width, 
+                top:e.pageY-height
+                });
+            });
+
+            $('.link1').mouseenter(_=> {
+                $('#image1').show();
+            });
+  
+            $('.link1').mouseleave(_=> {
+                $('#image1').hide();
+            });
+
+$(document).mousemove(function(e){
+            const height = $('#image2').height() / 10;
+            const width = $('#image2').width() / 28;
+                $("#image2").css({
+                left:e.pageX-width, 
+                top:e.pageY-height
+                });
+            });
+
+            $('.link2').mouseenter(_=> {
+                $('#image2').show();
+            });
+  
+            $('.link2').mouseleave(_=> {
+                $('#image2').hide();
+            });
+
+$(document).mousemove(function(e){
+            const height = $('#image3').height() / 10;
+            const width = $('#image3').width() / 28;
+                $("#image3").css({
+                left:e.pageX-width, 
+                top:e.pageY-height
+                });
+            });
+
+            $('.link3').mouseenter(_=> {
+                $('#image3').show();
+            });
+  
+            $('.link3').mouseleave(_=> {
+                $('#image3').hide();
+            });
+
+$(document).mousemove(function(e){
+            const height = $('#image4').height() / 10;
+            const width = $('#image4').width() / 28;
+                $("#image4").css({
+                left:e.pageX-width, 
+                top:e.pageY-height
+                });
+            });
+
+            $('.link4').mouseenter(_=> {
+                $('#image4').show();
+            });
+  
+            $('.link4').mouseleave(_=> {
+                $('#image4').hide();
+            });
+
+
+$(document).mousemove(function(e){
+            const height = $('#image5').height() / 10;
+            const width = $('#image5').width() / 28;
+                $("#image5").css({
+                left:e.pageX-width, 
+                top:e.pageY-height
+                });
+            });
+
+            $('.link5').mouseenter(_=> {
+                $('#image5').show();
+            });
+  
+            $('.link5').mouseleave(_=> {
+                $('#image5').hide();
+            });
+
+$(document).mousemove(function(e){
+            const height = $('#image6').height() / 10;
+            const width = $('#image6').width() / 28;
+                $("#image6").css({
+                left:e.pageX-width, 
+                top:e.pageY-height
+                });
+            });
+
+            $('.link6').mouseenter(_=> {
+                $('#image6').show();
+            });
+  
+            $('.link6').mouseleave(_=> {
+                $('#image6').hide();
+            });
